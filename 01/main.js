a56cf9dd0696d878eaac27b3bc79f0f5cddbf585
Rocky <rockygitt@gmail.com>
2020-12-26 19:35:43 +0100

    AoC Problem 01

---
+++ 01/main.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const getFuel = mass => {
+  const result = Math.floor(Number(mass) / 3) - 2
+
+  return result > 0 ? result : 0
+}
+
+const getInput = async () => {
+  return new Promise(resolve => {
+    const lines = []
+
+    readline.createInterface({ input: fs.createReadStream('./input.txt') })
+      .on('line', line => lines.push(line))
+      .on('close', () => resolve(lines))
+  })
+}
+
+const main = async () => {
+  const masses = await getInput()
+
+  let fuel = masses.map(getFuel)
+  let result = fuel.reduce((t, f) => t + f)
+
+  console.log(`Sum of the fuel requirements (1): ${result}`)
+
+  const totalFuel = []
+
+  while (fuel.reduce((t, f) => t + f) != 0) {
+    totalFuel.push(fuel)
+    fuel = fuel.map(getFuel)
+  }
+
+  result = totalFuel.map(f => f.reduce((t, f) => t + f)).reduce((t, f) => t + f)
+
+  console.log(`Sum of the total fuel requirements (2): ${result}`)
+}
+
+main()
