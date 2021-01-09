d93b1810d4d39802c133c82a165ea67075ff4445
Rocky <rockygitt@gmail.com>
2021-01-09 20:06:01 +0100

    AoC Problem 08

---
+++ day_08/main.js
@@ -0,0 +1,95 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const getInput = async () => {
+  return new Promise(resolve => {
+    const lines = []
+
+    readline
+      .createInterface({ input: fs.createReadStream('./input.txt') })
+      .on('line', line => lines.push(line))
+      .on('close', () => (lines.length === 1 ? resolve(lines[0]) : resolve(lines)))
+  })
+}
+
+const countPixels = (pixel, layer) => {
+  return layer.map(row => row.filter(p => p === pixel).length).reduce((t, n) => t + n)
+}
+
+const main = async () => {
+  const pixelStream = await getInput()
+  const dimensions = [25, 6]
+
+  pixels = pixelStream.split('').map(Number)
+
+  const layers = []
+
+  const totalLayers = pixels.length / (dimensions[0] * dimensions[1])
+
+  for (let k = 0; k < totalLayers; k++) {
+    const layer = []
+
+    for (let j = 0; j < dimensions[1]; j++) {
+      const row = []
+
+      for (let i = 0; i < dimensions[0]; i++) {
+        row.push(pixels[(k * dimensions[1] + j) * dimensions[0] + i])
+      }
+
+      layer.push(row)
+    }
+
+    layers.push(layer)
+  }
+
+  let wantedLayer
+  let minCount = dimensions[0] * dimensions[1]
+
+  for (const layer of layers) {
+    const count = countPixels(0, layer)
+
+    if (minCount > count) {
+      wantedLayer = layer
+      minCount = count
+    }
+  }
+
+  console.log(
+    `Number of pixels 1 on the wanted layer (1): ${
+      countPixels(1, wantedLayer) * countPixels(2, wantedLayer)
+    }`
+  )
+
+  const image = []
+
+  for (let j = 0; j < dimensions[1]; j++) {
+    const row = []
+
+    for (let i = 0; i < dimensions[0]; i++) {
+      row.push('#')
+    }
+
+    image.push(row)
+  }
+
+  const pointer = [0, 0]
+
+  while (countPixels('#', image)) {
+    const pixels = layers.map(layer => layer[pointer[1]][pointer[0]])
+    image[pointer[1]][pointer[0]] = pixels.find(pixel => pixel !== 2)
+    pointer[0]++
+
+    if (pointer[0] === dimensions[0]) {
+      pointer[0] = 0
+      pointer[1]++
+
+      if (pointer[1] === dimensions[1]) break
+    }
+  }
+
+  console.log(`Image decoded mesage (2): ${decode(image)}`)
+}
+
+const decode = image => image.reduce((d, row) => `${d}\n${row.join('').replace(/0/g, ' ')}`, '\n')
+
+main()
